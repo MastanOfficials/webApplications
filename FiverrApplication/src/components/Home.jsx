31880a37292776f7898228505842c33a27e158e3
Mastan <komodheen.mastan@krazybee.com>
2023-05-31 14:52:25 +0530

    added components

--- FiverrApplication/src/components/Home.jsx
+++ FiverrApplication/src/components/Home.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import { cards } from '../data'
-import CatCard from './catcard/CatCard'
-import Features from './features/Features'
-import Slide from './slide/Slide'
-import TrustedBy from './trustedby/TrustedBy'
+import React from "react";
+import { cards } from "../data";
+import CatCard from "./catcard/CatCard";
+import Features from "./features/Features";
+import Slide from "./slide/Slide";
+import TrustedBy from "./trustedby/TrustedBy";
 
 const Home = () => {
   return (
     <div>
-      <Features/>
-      <TrustedBy/>
+      <Features />
+      <TrustedBy />
       <Slide slidesToShow={5} arrowsScroll={5}>
-        {
-          cards?.map(card =>{
-            <CatCard key={card?.id} items={card} />
-          })
-        }
+        {cards?.map((cards) => {
+          <>
+            console.log(cards);
+            <CatCard key={cards?.id} items={cards} />
+          </>;
+        })}
       </Slide>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
